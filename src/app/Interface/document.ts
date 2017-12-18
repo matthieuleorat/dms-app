1e289d2dec0113073a87ea727d681a175bf980b0
Matthieu Leorat <matthieu.leorat@protonmail.com>
2017-12-18 11:39:28 +0100

    - Display document thumbnail in document list

--- src/app/Interface/document.ts
+++ src/app/Interface/document.ts
@@ -1,6 +1,7 @@
 export interface Document {
     id?: Number, // Id          document.id
     name: string, // Given name document.name
+    thumbnail: string,
     //path: string, // File path  document.path
     //file: string, // File name  document.file
 }
